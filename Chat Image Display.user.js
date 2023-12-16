3138107d676a5a752beaf128c79d3e8bb0be13fd
c0IIwr <96628073+c0IIwr@users.noreply.github.com>
2023-12-16 13:47:51 +0300

    Update Chat Image Display.user.js

--- Chat Image Display.user.js
+++ Chat Image Display.user.js
@@ -13,56 +13,48 @@
 (function() {
     'use strict';
 
-    function processVKPlayChatImages(node) {
-        const messageElements = node.querySelectorAll('.ChatMessage_message_r1jzC a');
-        processChatImages(messageElements);
-    }
-
-    function processBoostyChatImages(node) {
-        const messageElements = node.querySelectorAll('.ChatMessage_text_sXPvk a');
-        processChatImages(messageElements);
-    }
-
-    // Переиспользуемый обработчик изображений для обоих сайтов
-    function processChatImages(messageElements) {
+    // Общая функция для обработки изображений в чате
+    function processChatImages(node, messageSelector) {
+        const messageElements = node.querySelectorAll(messageSelector);
         messageElements.forEach(link => {
             const matched = link.href.match(/(\.(jpeg|jpg|gif|png|webp))($|\?.*$|#.*$)/i);
             if (matched !== null) {
-                // Общая логика обработки изображений...
+                // здесь логика обработки ссылки и создание элемента изображения
+                // ...
+                // Пропустим повтор кода для компактности
+                // ...
             }
         });
     }
 
-    // Обозреватель для VK Play
-    const vkChatObserver = new MutationObserver(mutations => {
-      // Логика обозревателя...
-    });
-    
-    // Обозреватель для Boosty
-    const boostyChatObserver = new MutationObserver(mutations => {
-      // Логика обозревателя...
-    });
+    // Определение сайта и соответствующих селекторов
+    let chatSelector, messageSelector;
+    if (location.href.includes('vkplay.live')) {
+        chatSelector = '.Chat_chat_x6IXr.Chat_root_tUBSs';
+        messageSelector = '.ChatMessage_message_r1jzC a';
+    } else if (location.href.includes('boosty.to')) {
+        chatSelector = '[data-test-id="CHAT:root"]';
+        messageSelector = '.ChatMessage_text_sXPvk a';
+    }
 
-    // Настройки для наблюдателей должны быть представлены в общем объекте, поскольку они идентичны
-    const observerConfig = { childList: true, subtree: true };
-    
-    // Обработка определения текущего сайта и запуск соответствующего кода
-    switch(window.location.host) {
-        case "vkplay.live": {
-            const vkChatElement = document.querySelector('.Chat_chat_x6IXr.Chat_root_tUBSs');
-            if (vkChatElement) {
-                // Запуск функциональности VK Play...
-            }
-            break;
+    // Наблюдатель за мутациями и запуск предварительной обработки
+    if (chatSelector) {
+        const chatObserver = new MutationObserver(mutations => {
+            mutations.forEach(mutation => {
+                if (mutation.type === 'childList') {
+                    mutation.addedNodes.forEach(node => processChatImages(node, messageSelector));
+                }
+            });
+        });
+
+        const config = { childList: true, subtree: true };
+        const chatElement = document.querySelector(chatSelector);
+
+        if (chatElement) {
+            processChatImages(chatElement, messageSelector);
+            chatObserver.observe(chatElement, config);
+        } else {
+            console.log('Chat container not found');
         }
-        case "boosty.to": {
-            const boostyChatElement = document.querySelector('[data-test-id="CHAT:root"]');
-            if (boostyChatElement) {
-                // Запуск функциональности Boosty...
-            }
-            break;
-        }
-        default:
-            console.log("This site is not supported by the Unified Chat Image Display script.");
     }
 })();
