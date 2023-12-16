3eb1c3525d241a9bb12a92fc333ceacdf6254b69
c0IIwr <96628073+c0IIwr@users.noreply.github.com>
2023-12-16 03:53:47 +0300

    Update Boosty Chat Image Display.user.js

--- Boosty Chat Image Display.user.js
+++ Boosty Chat Image Display.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Boosty Chat Image Display
-// @namespace    https://pastebin.com/F3uUEVxL
-// @version      1.2
+// @namespace    https://github.com/c0IIwr/Chat-Image-Display/raw/main/Boosty%20Chat%20Image%20Display.user.js
+// @version      1.3
 // @description  Display images directly in Boosty streams chat
 // @author       c0IIwr
 // @match        https://boosty.to/*/streams/video_stream
@@ -16,7 +16,7 @@
         const messageElements = node.querySelectorAll('.ChatMessage_text_sXPvk a');
         messageElements.forEach(link => {
             const matched = link.href.match(/(\.(jpeg|jpg|gif|png|webp))($|\?.*$|#.*$)/i);
-            if (matched != null) {
+            if (matched !== null) {
                 link.href = link.href.substring(0, link.href.indexOf(matched[1]) + matched[1].length);
                 link.textContent = link.href;
                 link.style.display = 'none';
@@ -25,10 +25,30 @@
                 image.style.maxWidth = '100%';
                 image.style.maxHeight = '322px';
                 image.style.display = 'block';
-                image.onclick = () => {
-                    navigator.clipboard.writeText(link.href).then(() => {
-                    }).catch(err => {
-                    });
+                image.style.cursor = 'pointer';
+
+                let clicks = 0;
+                image.onclick = (event) => {
+                    const doubleClickDelay = 400;
+                    clicks++;
+
+                    if (clicks === 1) {
+                        setTimeout(() => {
+                            if (clicks === 1) {
+                                navigator.clipboard.writeText(link.href)
+                                    .then(() => {
+                                    console.log('Link copied to clipboard: ' + link.href);
+                                })
+                                    .catch(err => {
+                                    console.error('Unable to copy link: ' + err);
+                                });
+                            }
+                            clicks = 0;
+                        }, doubleClickDelay);
+                    } else if (clicks === 2) {
+                        window.open(link.href, '_blank');
+                        clicks = 0;
+                    }
                 };
 
                 image.onload = () => {
@@ -54,7 +74,6 @@
 
     if (chatElement) {
         processChatImages(chatElement);
-
         chatObserver.observe(chatElement, config);
     } else {
         console.log('Chat container not found');
