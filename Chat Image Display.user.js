17e853c92aea52e4f5916c444186c23f5c2bf1c5
c0IIwr <96628073+c0IIwr@users.noreply.github.com>
2023-12-16 15:57:10 +0300

    Update Chat Image Display.user.js

--- Chat Image Display.user.js
+++ Chat Image Display.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Chat Image Display
 // @namespace    https://github.com/c0IIwr/Chat-Image-Display/raw/main/Chat%20Image%20Display.user.js
-// @version      1.3
+// @version      1.3.1
 // @description  Display images in the chat room
 // @description:ru Отображение изображений в чате
 // @author       c0IIwr
